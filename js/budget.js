e5019c7b34e9cc99c69ad6cd8025932de69e5c94
mahimur <hellomahimur@gmail.com>
2022-08-23 22:15:17 +0600

    total cost calculated

--- js/budget.js
+++ js/budget.js
@@ -5,8 +5,23 @@ function getInputFieldValueById(elementId) {
     return currentInputValue;
 }
 
-document.getElementById('btn-calc').addEventListener('click', function () {
+function playerExpenses() {
     const perPlayerBuget = getInputFieldValueById('pp-input');
     const playerExpenses = document.getElementById('player-expenses');
     playerExpenses.innerText = perPlayerBuget * 5;
+    return playerExpenses;
+}
+
+document.getElementById('btn-calc').addEventListener('click', function () {
+    playerExpenses();
+});
+
+document.getElementById('btn-total').addEventListener('click', function () {
+    const managerCost = getInputFieldValueById('manager-input');
+    const coachCost = getInputFieldValueById('coach-input');
+    const playerExpensesTotalString = playerExpenses().innerText;
+    const playerExpensesTotal = parseFloat(playerExpensesTotalString);
+    const totalExpenses = playerExpensesTotal + managerCost + coachCost;
+    const totalExpensesField = document.getElementById('total-expenses');
+    totalExpensesField.innerText = totalExpenses;
 });
